e123ef74e0e0a483a2583ef493f27a07a38dace9
itsmylifestyle <hahaii00@mail.ru>
2022-10-30 20:25:44 +0600

    Initial Commit

---
+++ MoneyCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoneyCalculator
+//
+//  Created by Айбек on 30.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyCalculator/MoneyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyCalculatorApp.swift
+//  MoneyCalculator
+//
+//  Created by Айбек on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
